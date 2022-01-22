81145a576bc3cca7ee9ae3493cd9d00a3f567f4d
claudia.davis <claudiacdavis@gmail.com>
2022-01-22 18:02:49 -0500

    color code now works

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,38 +17,119 @@ var today = moment().format("dddd, MMM Do YYYY");
 document.getElementById("currentDay").textContent = today;
 
 //click event for save button
-$(".saveBtn").on("click", function () {
-  var text = $(this).siblings(".description").val();
-  var time = $(this).parent().attr("id");
+$(document).ready(function () {
+  $(".saveBtn").on("click", function () {
+    // get textarea description
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
 
-  localStorage.setItem(time, text);
-});
-
-//different colored times
-function timeTracker() {
-  //get current number of hours.
-  var timeNow = moment().hour();
-
-  // loop over time blocks
-  $(".time-block").each(function () {
-    var blockTime = parseInt($(this).attr("id").split("hour")[1]);
-
-    // To check the time and add the classes for background indicators
-    if (blockTime < timeNow) {
-      $(this).removeClass("future");
-      $(this).removeClass("present");
-      $(this).addClass("past");
-    } else if (blockTime === timeNow) {
-      $(this).removeClass("past");
-      $(this).removeClass("future");
-      $(this).addClass("present");
-    } else {
-      $(this).removeClass("present");
-      $(this).removeClass("past");
-      $(this).addClass("future");
-    }
+    // set into localStorage
+    localStorage.setItem(time, text);
   });
-}
 
-// call function
-timeTracker();
+  // get from localStorage
+  $("#9 .description").val(localStorage.getItem("9"));
+  $("#10 .description").val(localStorage.getItem("10"));
+  $("#11 .description").val(localStorage.getItem("11"));
+  $("#12 .description").val(localStorage.getItem("12"));
+  $("#1 .description").val(localStorage.getItem("1"));
+  $("#2 .description").val(localStorage.getItem("2"));
+  $("#3 .description").val(localStorage.getItem("3"));
+  $("#4 .description").val(localStorage.getItem("4"));
+  $("#5 .description").val(localStorage.getItem("5"));
+
+  //converting time to variable
+
+  var h9 = 9;
+  var h10 = 10;
+  var h11 = 11;
+  var h12 = 12;
+  var h1 = 13;
+  var h2 = 14;
+  var h3 = 15;
+  var h4 = 16;
+  var h5 = 17;
+  var h6 = 18;
+
+  var currentHour = moment().format("H");
+
+  if (currentHour < h9) {
+    $("#9-am").addClass("future");
+  } else if (currentHour > h9) {
+    $("#9-am").addClass("past");
+  } else if ((currentHour = h9)) {
+    $("#9-am").addClass("present");
+  }
+
+  if (currentHour < h10) {
+    $("#10-am").addClass("future");
+  } else if (currentHour > h10) {
+    $("#10-am").addClass("past");
+  } else if ((currentHour = h10)) {
+    $("#10-am").addClass("present");
+  }
+
+  if (currentHour < h11) {
+    $("#11-am").addClass("future");
+  } else if (currentHour > h11) {
+    $("#11-am").addClass("past");
+  } else if ((currentHour = h11)) {
+    $("#11-am").addClass("present");
+  }
+
+  if (currentHour < h12) {
+    $("#12-pm").addClass("future");
+  } else if (currentHour > h12) {
+    $("#12-pm").addClass("past");
+  } else if ((currentHour = h12)) {
+    $("#12-pm").addClass("present");
+  }
+
+  if (currentHour < h1) {
+    $("#1-pm").addClass("future");
+  } else if (currentHour > h1) {
+    $("#1-pm").addClass("past");
+  } else if ((currentHour = h1)) {
+    $("#1-pm").addClass("present");
+  }
+
+  if (currentHour < h2) {
+    $("#2-pm").addClass("future");
+  } else if (currentHour > h2) {
+    $("#2-pm").addClass("past");
+  } else if ((currentHour = h2)) {
+    $("#2-pm").addClass("present");
+  }
+
+  if (currentHour < h3) {
+    $("3-pm").addClass("future");
+  } else if (currentHour > h3) {
+    $("#3-pm").addClass("past");
+  } else if ((currentHour = h3)) {
+    $("#3-pm").addClass("present");
+  }
+
+  if (currentHour < h4) {
+    $("#4-pm").addClass("future");
+  } else if (currentHour > h4) {
+    $("#4-pm").addClass("past");
+  } else if ((currentHour = h4)) {
+    $("#4-pm").addClass("present");
+  }
+
+  if (currentHour < h5) {
+    $("#5-pm").addClass("future");
+  } else if (currentHour > h5) {
+    $("#5-pm").addClass("past");
+  } else if ((currentHour = h5)) {
+    $("#5-pm").addClass("present");
+  }
+
+  if (currentHour < h6) {
+    $("#6-pm").addClass("future");
+  } else if (currentHour > h6) {
+    $("#6-pm").addClass("past");
+  } else if ((currentHour = h6)) {
+    $("#6-pm").addClass("present");
+  }
+});
